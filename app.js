5770a36c7ed35e0333eb2373036d8aaf4e6c965d
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2012-11-16 11:05:35 +0100

    remove routes

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -18,16 +16,16 @@ app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(require('less-middleware')({
+    src:  __dirname + '/less'
+  , dest: __dirname + '/public/stylesheets'
+  }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function () {
   app.use(express.errorHandler());
 });
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
