566d03d5de9e64d89703ccede5bb3889167b26e2
Oscar Söderlund <oscar.d.soderlund@gmail.com>
2012-11-20 21:41:19 +0100

    remove public folder

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-var socket = io.connect('http://localhost')
-  , $body = $('body');
-
-socket.on('update', function (data) {
-  $body.html(markdown.toHTML(data));
-  MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
-});
